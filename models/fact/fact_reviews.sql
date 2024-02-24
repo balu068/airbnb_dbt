72f9a0ac80d26386c95454ce54c36001990eff58
hnhonnguyen <vn3848587@gmail.com>
2024-02-24 21:30:49 +0700

    create surrogate key by using dbt_utils

--- models/fact/fact_reviews.sql
+++ models/fact/fact_reviews.sql
@@ -4,7 +4,10 @@
     on_schema_change = 'fail'
     )
 }}
-SELECT * FROM {{ ref('src_reviews') }}
+SELECT 
+{{dbt_utils.generate_surrogate_key(['listing_id','reviewer_name','review_date','review_text'])}} as review_id,
+*
+ FROM {{ ref('src_reviews') }}
 {% if is_incremental() %}
   where REVIEW_DATE >= coalesce((select max(REVIEW_DATE) from {{ this }}), '1900-01-01')
 {% endif %}
